3c2902e7f63300b13816c59320a923e17adcf5a9
c0ndii <105825543+c0ndii@users.noreply.github.com>
2023-11-19 17:25:48 +0100

    LogowanieBackEnd

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -7,22 +7,22 @@ namespace Find_H_er.Entities
         public int UserId { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Description { get; set; }
-        public string Sex { get; set; }
-        public string Image { get; set; }
-        public List<Chat> Chats { get; set; }
-        public int MatchFormId { get; set; }
-        public virtual MatchForm MatchForm { get; set; }
-        public List<Pinch> Pinches { get; set; }
-        public int ForYouId { get; set; }
-        public virtual ForYou ForYou { get; set; }
-        public List<Message> Messages { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Description { get; set; }
+        public string? Sex { get; set; }
+        public string? Image { get; set; }
+        public List<Chat>? Chats { get; set; }
+        public int? MatchFormId { get; set; }
+        public virtual MatchForm? MatchForm { get; set; }
+        public List<Pinch>? Pinches { get; set; }
+        public int? ForYouId { get; set; }
+        public virtual ForYou? ForYou { get; set; }
+        public List<Message>? Messages { get; set; }
         //public  zaczepka
-        public List<Preference> Preferences { get; set; }
-        public List<Interest> Interests { get; set; }
-        public int RoleId { get; set; }
-        public virtual Role Role { get; set; }
+        public List<Preference>? Preferences { get; set; }
+        public List<Interest>? Interests { get; set; }
+        public int? RoleId { get; set; }
+        public virtual Role? Role { get; set; }
     }
 }
