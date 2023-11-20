add2d502b0e22532ff5e30352b37dff8ebe012ef
slik margor <slikmargor@gmail.com>
2023-11-20 16:01:47 +0100

    feature(SignIn): create SignIn, SignUp pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Welcome } from './routes/Welcome'
-import { Layout } from "./components/Layout";
-import { AccountView } from "./view/AccountView";
-import { PreferencesView } from "./view/PreferencesView";
-import { InterestsView } from "./view/InterestsView";
-import { SettingsView } from "./view/SettingsView";
+import { Layout } from './components/Layout'
+import { AccountView } from './view/AccountView'
+import { PreferencesView } from './view/PreferencesView'
+import { InterestsView } from './view/InterestsView'
+import { SettingsView } from './view/SettingsView'
+import { SignIn } from './routes/SignIn'
+import { SignUp } from './routes/SignUp'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Welcome />} />
-      <Route path="/SignIn" element={<Welcome />} />
-        <Route path="/app" element={<Layout />}>
-          <Route path="Account" element={<AccountView />}/>
-          <Route path="Preferences" element={<PreferencesView />}/>
-          <Route path="Interests" element={<InterestsView />}/>
-          <Route path="Settings" element={<SettingsView />}/>
-        </Route>
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/app" element={<Layout />}>
+        <Route path="Account" element={<AccountView />} />
+        <Route path="Preferences" element={<PreferencesView />} />
+        <Route path="Interests" element={<InterestsView />} />
+        <Route path="Settings" element={<SettingsView />} />
+      </Route>
     </Routes>
   )
-};
+}
 
-export default App;
+export default App
---
+++ frontend/src/routes/SignUp.tsx
@@ -0,0 +1,11 @@
+import { BackgroundContainer } from '../components/Common/BackgroundContainer'
+import { FormContainer } from '../components/Common/FormContainer'
+import { SignUpForm } from '../components/SignUp/SignUpForm'
+
+export const SignUp = () => {
+  return (
+    <BackgroundContainer>
+      <SignUpForm />
+    </BackgroundContainer>
+  )
+}
