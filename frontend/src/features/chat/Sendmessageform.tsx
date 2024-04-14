cc75ff7163d5bcacdc7717988f7ab77874b0e419
Dawid <dawid1234x@wp.pl>
2024-04-14 13:28:19 +0200

    tettetetemp

--- frontend/src/features/chat/Sendmessageform.tsx
+++ frontend/src/features/chat/Sendmessageform.tsx
@@ -1,4 +1,4 @@
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core'
+import { TextInput, Button, Group, Box } from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { t } from 'i18next'
 
