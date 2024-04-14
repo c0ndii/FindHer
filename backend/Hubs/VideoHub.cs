a65cf1e12ee2d9351c5cf363ff18f7ae55c49620
c0ndii <105825543+c0ndii@users.noreply.github.com>
2024-04-14 23:01:07 +0200

    fix(backend): Videochat

--- backend/Hubs/VideoHub.cs
+++ backend/Hubs/VideoHub.cs
@@ -1,7 +1,6 @@
 ﻿using Find_H_er.Entities;
 using Find_H_er.Exceptions;
 using Find_H_er.Models;
-using Find_H_er.Models.VideoChat;
 using Find_H_er.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
@@ -13,171 +12,42 @@ using TableDependency.SqlClient.Base.Messages;
 
 namespace Find_H_er.Hubs
 {
-    public interface IConnectionHub
-    {
-        Task UpdateUserList(List<User> userList);
-        Task CallAccepted(User acceptingUser);
-        Task CallDeclined(User decliningUser, string reason);
-        Task IncomingCall(User callingUser);
-        Task ReceiveSignal(User signalingUser, string signal);
-        Task CallEnded(User signalingUser, string signal);
-    }
-    public class VideoHub : Hub<IConnectionHub>
+    public class VideoHub : Hub
     {
         private readonly AppDbContext _context;
-        private readonly List<UserCall> _userCalls;
-        private readonly List<CallOffer> _callOfferList;
 
         public VideoHub(AppDbContext context)
         {
             _context = context;
-            _userCalls = new List<UserCall>();
-            _callOfferList = new List<CallOffer>();
         }
-        public async Task CallUser(int targetUserId)
+        public async Task TurnOnCamera(int targetId)
         {
-            var targetConnectionId = await _context.Users.SingleOrDefaultAsync(x => x.UserId == targetUserId);
-            if(targetConnectionId is null)
-            {
-                throw new NotFoundException("User not found");
-            }
-            var caller = await _context.Users.SingleOrDefaultAsync(x => x.VideoChatConnectionId == Context.ConnectionId);
-            var target = await _context.Users.SingleOrDefaultAsync(x => x.VideoChatConnectionId == targetConnectionId.VideoChatConnectionId);
-
-            // Make sure the person we are trying to call is still here
-            if (target == null)
-            {
-                // If not, let the caller know
-                await Clients.Caller.CallDeclined(targetConnectionId, "The user you called has left.");
-                return;
-            }
-
-            // And that they aren't already in a call
-            if (GetUserCall(target.VideoChatConnectionId) != null)
-            {
-                await Clients.Caller.CallDeclined(targetConnectionId, string.Format("{0} is already in a call.", target.Name));
-                return;
-            }
-
-            // They are here, so tell them someone wants to talk
-            await Clients.Client(targetConnectionId.VideoChatConnectionId).IncomingCall(caller);
-
-            // Create an offer
-            _callOfferList.Add(new CallOffer
-            {
-                Caller = caller,
-                Target = target
-            });
-        }
-        public async Task AnswerCall(bool acceptCall, User targetConnectionId)
-        {
-            var caller = await _context.Users.SingleOrDefaultAsync(x => x.VideoChatConnectionId == Context.ConnectionId);
-            var target = await _context.Users.SingleOrDefaultAsync(x => x.VideoChatConnectionId == targetConnectionId.VideoChatConnectionId);
-            if (caller == null)
-            {
-                return;
-            }
-            if (target == null)
-            {
-                await Clients.Caller.CallEnded(targetConnectionId, "The other user in your call has left.");
-                return;
-            }
-            if (acceptCall == false)
-            {
-                await Clients.Client(targetConnectionId.VideoChatConnectionId).CallDeclined(caller, string.Format("{0} did not accept your call.", caller.Name));
-                return;
-            }
-            var offerCount = _callOfferList.RemoveAll(c => c.Target.VideoChatConnectionId == caller.VideoChatConnectionId && c.Caller.VideoChatConnectionId == target.VideoChatConnectionId);
-            if (offerCount < 1)
-            {
-                await Clients.Caller.CallEnded(targetConnectionId, string.Format("{0} has already hung up.", target.Name));
-                return;
-            }
-            if (GetUserCall(target.VideoChatConnectionId) != null)
-            {
-                await Clients.Caller.CallDeclined(targetConnectionId, string.Format("{0} chose to accept someone elses call instead of yours :(", target.Name));
-                return;
-            }
-            _callOfferList.RemoveAll(c => c.Caller.VideoChatConnectionId == target.VideoChatConnectionId);
-            _userCalls.Add(new UserCall
-            {
-                Users = new List<User> { caller, target }
-            });
-            await Clients.Client(targetConnectionId.VideoChatConnectionId).CallAccepted(caller);
-        }
-        public async Task HangUp()
-        {
-            var caller = await _context.Users.SingleOrDefaultAsync(x => x.VideoChatConnectionId == Context.ConnectionId);
-
-            if (caller == null)
-            {
-                return;
-            }
-
-            var currentCall = GetUserCall(caller.VideoChatConnectionId);
-
-            if (currentCall != null)
-            {
-                foreach (var user in currentCall.Users.Where(u => u.VideoChatConnectionId != caller.VideoChatConnectionId))
-                {
-                    await Clients.Client(user.VideoChatConnectionId).CallEnded(caller, string.Format("{0} has hung up.", caller.Name));
-                }
-                currentCall.Users.RemoveAll(u => u.VideoChatConnectionId == caller.VideoChatConnectionId);
-                if (currentCall.Users.Count < 2)
-                {
-                    _userCalls.Remove(currentCall);
-                }
-            }
-
-            // Remove all offers initiating from the caller
-            _callOfferList.RemoveAll(c => c.Caller.VideoChatConnectionId == caller.VideoChatConnectionId);
-        }
-        private UserCall GetUserCall(string connectionId)
-        {
-            var matchingCall = _userCalls.SingleOrDefault(x => x.Users.SingleOrDefault(y => y.VideoChatConnectionId == connectionId) != null);
-            return matchingCall;
-        }
-        public async Task SaveUserConnection(int senderId)
-        {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserId == senderId);
+            var userEmail = Context.User.Identity.Name;
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.Email == userEmail);
             if (user is null)
             {
                 throw new NotFoundException("User not found");
             }
-            var videoChatConnectionId = Context.ConnectionId;
-            user.VideoChatConnectionId = videoChatConnectionId;
-            _context.Update(user);
-            await _context.SaveChangesAsync();
-        }
-        public async Task SendSignal(string signal, int targetConnectionId)
-        {
-            var callingUser = await _context.Users.SingleOrDefaultAsync(u => u.VideoChatConnectionId == Context.ConnectionId);
-            var targetUser = await _context.Users.SingleOrDefaultAsync(u => u.UserId == targetConnectionId);
-
-            // Make sure both users are valid
-            if (callingUser == null || targetUser == null)
+            var targetUser = await _context.Users.SingleOrDefaultAsync(x => x.UserId == targetId);
+            if(targetUser is null)
             {
-                return;
+                throw new NotFoundException("User not found");
             }
-
-            // Make sure that the person sending the signal is in a call
-            var userCall = GetUserCall(callingUser.VideoChatConnectionId);
-
-            // ...and that the target is the one they are in a call with
-            if (userCall != null && userCall.Users.Exists(u => u.VideoChatConnectionId == targetUser.VideoChatConnectionId))
-            {
-                // These folks are in a call together, let's let em talk WebRTC
-                await Clients.Client(targetUser.VideoChatConnectionId).ReceiveSignal(callingUser, signal);
-            }
+            await Clients.Client(targetUser.VideoChatConnectionId).SendAsync("camera-on", user.VideoChatConnectionId);
         }
         public override Task OnConnectedAsync()
         {
+            var email = Context.GetHttpContext().User.Identity.Name;
+            var user =  _context.Users.SingleOrDefault(x => x.Email == email);
+            user.VideoChatConnectionId = Context.ConnectionId;
+            _context.Update(user);
+            _context.SaveChanges();
             return base.OnConnectedAsync();
         }
-        public override async Task OnDisconnectedAsync(Exception exception)
+        public override Task OnDisconnectedAsync(Exception? exception)
         {
-            await HangUp();
-            await base.OnDisconnectedAsync(exception);
+            return base.OnDisconnectedAsync(exception);
         }
+
     }
 }
--- backend/Models/VideoChat/UserCall.cs
+++
@@ -1,9 +0,0 @@
-﻿using Find_H_er.Entities;
-namespace Find_H_er.Models.VideoChat
-{
-    public class UserCall
-    {
-        public int UserCallId { get; set; }
-        public List<User> Users { get; set; }
-    }
-}
