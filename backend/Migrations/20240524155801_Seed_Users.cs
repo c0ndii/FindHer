b9dfae72b3acb41b2adb154f86452840e62da15d
slik margor <slikmargor@gmail.com>
2024-05-25 12:23:05 +0200

    feat: added sql user migration seed

---
+++ backend/Migrations/20240524155801_Seed_Users.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using Find_H_er.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Find_H_er.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_Users : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var passwordHasher = new PasswordHasher<User>();
+
+            var users = new List<User>
+            {
+                new User
+                {
+                    UserId = 1,
+                    Email = "user1@wp.pl",
+                    Name = "Adam",
+                    VerificationToken = "randomToken1",
+                    VideoChatConnectionId = Guid.NewGuid().ToString(),
+                    RoleId = 1 
+                },
+                new User
+                {
+                    UserId = 2,
+                    Email = "user2@wp.pl",
+                    Name = "Andrzej",
+                    VerificationToken = "randomToken2",
+                    VideoChatConnectionId = Guid.NewGuid().ToString(),
+                    RoleId = 2
+                },
+                new User
+                {
+                    UserId = 3,
+                    Email = "user3@wp.pl",
+                    Name = "Beata",
+                    VerificationToken = "randomToken3",
+                    VideoChatConnectionId = Guid.NewGuid().ToString(),
+                    RoleId = 1
+                },
+                new User
+                {
+                    UserId = 4,
+                    Email = "user4@wp.pl",
+                    Name = "Cecylia",
+                    VerificationToken = "randomToken4",
+                    VideoChatConnectionId = Guid.NewGuid().ToString(),
+                    RoleId = 1 
+                }
+            };
+
+            foreach (var user in users)
+            {
+                user.PasswordHash = passwordHasher.HashPassword(user, "test12345");
+            }
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("SET IDENTITY_INSERT Users ON;");
+
+            foreach (var user in users)
+            {
+                sb.AppendLine(
+                $"""
+                    INSERT INTO Users (UserId, Email, PasswordHash, Name, VerificationToken, RoleId, MatchFormScore, VideoChatConnectionId) 
+                    VALUES ({user.UserId}, '{user.Email}', '{user.PasswordHash}', '{user.Name}', '{user.VerificationToken}', 
+                            {user.RoleId}, {user.MatchFormScore ?? 50}, '{user.VideoChatConnectionId}');
+                """);
+                
+                sb.AppendLine(
+                $"""
+                    INSERT INTO MatchForms 
+                    VALUES ({user.UserId}, {user.UserId});
+                """);
+            }
+
+            sb.AppendLine("SET IDENTITY_INSERT Users OFF;");
+                
+            sb.AppendLine(
+                """
+                DECLARE @QuestionIds TABLE (QuestionId INT);
+            
+                INSERT INTO @QuestionIds (QuestionId)
+                SELECT QuestionId FROM Questions;
+
+                DECLARE @MatchFormId INT;
+
+                DECLARE MatchFormCursor CURSOR FOR
+                SELECT MatchFormId FROM MatchForms;
+
+                OPEN MatchFormCursor;
+
+                FETCH NEXT FROM MatchFormCursor INTO @MatchFormId;
+
+                WHILE @@FETCH_STATUS = 0
+                BEGIN
+                INSERT INTO MatchFormQuestion (MatchFormsMatchFormId, QuestionsQuestionId)
+                SELECT @MatchFormId, QuestionId FROM @QuestionIds;
+
+                FETCH NEXT FROM MatchFormCursor INTO @MatchFormId;
+                END
+
+                CLOSE MatchFormCursor;
+                DEALLOCATE MatchFormCursor;
+            """);
+            
+            migrationBuilder.Sql(sb.ToString());
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ backend/Migrations/20240525095227_Seed_Matches.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using Find_H_er.Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Find_H_er.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_Matches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var matches = new List<Match>
+            {
+                new Match { MatchId = 1, ViewerId = 1, ViewedId = 2, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+                new Match { MatchId = 2, ViewerId = 1, ViewedId = 3, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+                new Match { MatchId = 3, ViewerId = 1, ViewedId = 4, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+                new Match { MatchId = 5, ViewerId = 2, ViewedId = 3, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+                new Match { MatchId = 6, ViewerId = 2, ViewedId = 4, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+                new Match { MatchId = 9, ViewerId = 3, ViewedId = 4, Cancelled = false, MatchedViewer = false, MatchedViewed = false, Matched = false },
+            };
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("SET IDENTITY_INSERT Matches ON;");
+
+            foreach (var match in matches)
+            {
+                sb.AppendLine($@"
+                    INSERT INTO Matches (MatchId, ViewerId, ViewedId, Cancelled, MatchedViewer, MatchedViewed, Matched) 
+                    VALUES ({match.MatchId}, {match.ViewerId}, {match.ViewedId}, {Convert.ToInt32(match.Cancelled)}, 
+                            {Convert.ToInt32(match.MatchedViewer)}, {Convert.ToInt32(match.MatchedViewed)}, {Convert.ToInt32(match.Matched)});
+                ");
+            }
+
+            sb.AppendLine("SET IDENTITY_INSERT Matches OFF;");
+
+            migrationBuilder.Sql(sb.ToString());
+    
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
