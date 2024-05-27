ca80db35c3132f7c8535d06665646e0390ba3732
rapscallion <b.dudel@protonmail.com>
2024-05-27 17:26:11 +0200

    matchform translation

--- frontend/src/features/matchForm/MatchForm.tsx
+++ frontend/src/features/matchForm/MatchForm.tsx
@@ -44,71 +44,81 @@ export const MatchForm = () => {
         }}
       >
         <Stepper active={active} onStepClick={setActive} color="red">
-          <Stepper.Step label={t('matchForm.category1.title')}>
+          <Stepper.Step label={t('matchForm.category1')}>
             <Box
               style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}
             >
               <Select
                 key={questions?.questions.at(0)?.questionId}
-                label={questions?.questions.at(0)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(0)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(0)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question1')}
               />
               <Select
                 key={questions?.questions.at(1)?.questionId}
-                label={questions?.questions.at(1)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(1)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(1)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question2')}
               />
               <Select
                 key={questions?.questions.at(2)?.questionId}
-                label={questions?.questions.at(2)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(2)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(2)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question3')}
               />
               <Select
                 key={questions?.questions.at(3)?.questionId}
-                label={questions?.questions.at(3)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(3)?.questionContent}`
+                )}
                 clearable
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 data={
                   questions?.questions.at(3)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question4')}
               />
               <Select
                 key={questions?.questions.at(4)?.questionId}
-                label={questions?.questions.at(4)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(4)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(4)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question5')}
@@ -124,71 +134,81 @@ export const MatchForm = () => {
             </Group>
           </Stepper.Step>
 
-          <Stepper.Step label={t('matchForm.category2.title')}>
+          <Stepper.Step label={t('matchForm.category2')}>
             <Box
               style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}
             >
               <Select
                 key={questions?.questions.at(5)?.questionId}
-                label={questions?.questions.at(5)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(5)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(5)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question6')}
               />
               <Select
                 key={questions?.questions.at(6)?.questionId}
-                label={questions?.questions.at(6)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(6)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(6)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question7')}
               />
               <Select
                 key={questions?.questions.at(7)?.questionId}
-                label={questions?.questions.at(7)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(7)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(7)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question8')}
               />
               <Select
                 key={questions?.questions.at(8)?.questionId}
-                label={questions?.questions.at(8)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(8)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(8)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question9')}
               />
               <Select
                 key={questions?.questions.at(9)?.questionId}
-                label={questions?.questions.at(9)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(9)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(9)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question10')}
@@ -204,71 +224,81 @@ export const MatchForm = () => {
             </Group>
           </Stepper.Step>
 
-          <Stepper.Step label="Kategoria 3">
+          <Stepper.Step label={t('matchForm.category3')}>
             <Box
               style={{ display: 'flex', flexDirection: 'column', gap: '15px' }}
             >
               <Select
                 key={questions?.questions.at(10)?.questionId}
-                label={questions?.questions.at(10)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(10)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(10)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question11')}
               />
               <Select
                 key={questions?.questions.at(11)?.questionId}
-                label={questions?.questions.at(11)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(11)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(11)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question12')}
               />
               <Select
                 key={questions?.questions.at(12)?.questionId}
-                label={questions?.questions.at(12)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(12)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(12)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question13')}
               />
               <Select
                 key={questions?.questions.at(13)?.questionId}
-                label={questions?.questions.at(13)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(13)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(13)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question14')}
               />
               <Select
                 key={questions?.questions.at(14)?.questionId}
-                label={questions?.questions.at(14)?.questionContent}
+                label={t(
+                  `matchForm.${questions?.questions.at(14)?.questionContent}`
+                )}
                 placeholder={t('matchForm.answerFieldPlaceholder')}
                 clearable
                 data={
                   questions?.questions.at(14)?.answers.map((answer) => ({
                     value: answer.answerWeight.toString(),
-                    label: answer.answerContent,
+                    label: t(`matchForm.${answer.answerContent}`),
                   })) || []
                 }
                 {...form.getInputProps('question15')}
